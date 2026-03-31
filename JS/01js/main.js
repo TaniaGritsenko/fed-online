82d5eeaff3da3df0567952a3d23028b146a2b04f
TaniaGritsenko <gruzenko.t@ukr.net>
2026-03-31 11:43:51 +0300

    01js

---
+++ JS/01js/main.js
@@ -0,0 +1,8 @@
+let userAgeValue = 33;
+let userNameNickname = "Tania"; 
+
+userAgeValue = 30;
+userNameNickname = "Tina";
+
+console.log("Age:", userAgeValue);
+console.log("Nickname:", userNameNickname);
