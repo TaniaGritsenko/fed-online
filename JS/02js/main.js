e065c16d6c80bb21698559dfab78fba6cc2bfb17
TaniaGritsenko <gruzenko.t@ukr.net>
2026-03-31 20:54:57 +0300

    02js

---
+++ JS/02js/main.js
@@ -0,0 +1,26 @@
+let result1 = ( 5 + 3 ) * 2 - 7;
+console.log('result1 = ( 5 + 3 ) * 2 - 7 =', result1);
+
+let result2 = ( 10 / 2 ) ** 2;
+console.log('result2 = ( 10 / 2 ) ** 2 =', result2);
+
+let result3 = 2 * ( 4 + 3 ) - ( 8 / 2 );
+console.log('result3 = 2 * ( 4 + 3 ) - ( 8 / 2 ) =', result3);
+
+let result4 = ( 15 % 4 ) + 5;
+console.log('result4 = ( 15 %  4) + 5 =', result4);
+
+let result5 = 3 - ( 2 ** 4 );
+console.log('result5 = 3 - ( 2 ** 4 ) =', result5);
+
+let firstNumber = 8;
+let secondNumber = 2;
+
+console.log('firstNumber =', firstNumber);
+console.log('secondNumber =', secondNumber);
+console.log('firstNumber + secondNumber =', firstNumber + secondNumber);
+console.log('firstNumber - secondNumber =', firstNumber - secondNumber);
+console.log('firstNumber * secondNumber =', firstNumber * secondNumber);
+console.log('firstNumber / secondNumber =', firstNumber / secondNumber);
+console.log('firstNumber % secondNumber =', firstNumber % secondNumber);
+console.log('firstNumber ** secondNumber =', firstNumber ** secondNumber);
