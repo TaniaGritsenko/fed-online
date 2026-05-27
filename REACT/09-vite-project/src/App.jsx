3ea1ace8a15c47a13d66d11f29f8200988a924e6
TaniaGritsenko <gruzenko.t@ukr.net>
2026-05-27 18:11:58 +0300

    09react

---
+++ REACT/09-vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import UseStateComponent from "./Components/UseStateComponent"
+
+function App() {
+  return (
+    <>
+      <UseStateComponent></UseStateComponent>
+    </>
+  )
+}
+
+export default App
